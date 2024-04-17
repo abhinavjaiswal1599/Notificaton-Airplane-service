29c64e9c330eba804a8a34dc888c770a4247b58f
Abhinav Jaiswal <abhinavjaiswal1599@gmail.com>
2024-04-17 17:49:38 +0530

    Basic sequelize setup

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const v1Routes=require('./v1')
+const v1Routes = require('./v1');
 const router = express.Router();
 
-router.use('v1', v1Routes)
+// Correct usage: provide the route path before the middleware function
+router.use('v1', v1Routes);
 
-
-module.exports={
+module.exports = {
     router
-}
+};
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,6 +1,6 @@
 const express= require('express');  
 
-const {InfoController}  = require('../../controllers').InfoController
+const {InfoController}  = require('../../controllers')
 const router= express.Router();
 
 router.get('/info',InfoController.info);
